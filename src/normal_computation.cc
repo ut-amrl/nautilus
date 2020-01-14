973657d71d7f83fe6f04a477892ba8008d1f2fdc
TheCynosure <jack.github@gmail.com>
2020-01-14 13:45:20 -0500

    NormalComputationInitial

---
+++ src/normal_computation.cc
@@ -0,0 +1,88 @@
+#include <cstdlib>
+#include <time.h>
+#include <vector>
+
+#include "Eigen/Dense"
+
+#include "./normal_computation.h"
+#include "./kdtree.h"
+
+/* Normal computation is based on this paper
+ * http://imagine.enpc.fr/~marletr/publi/SGP-2012-Boulch-Marlet.pdf
+ * Fast and Robust Normal Estimation for Point Cloudswith Sharp Features
+ * By Boulch et al.
+ */
+
+using std::vector;
+using NormalComputation::CircularHoughAccumulator;
+
+#define NEIGHBORHOOD_SIZE 0.25
+#define NEIGHBORHOOD_STEP_SIZE 0.1
+#define MEAN_DISTANCE 0.1
+#define LIMIT_CORRECT_PROBABILITY 0.95
+#define BIN_NUMBER 32
+
+inline Vector2f GetNormalFromAngle(double angle) {
+  Eigen::Matrix2f rot = Eigen::Rotation2Df(angle).getRotationMatrix();
+  return rot * Vector2f(1, 0);
+}
+
+inline size_t SampleLimit(size_t bin_number) {
+  return (1 / (2.0 * MEAN_DISTANCE * MEAN_DISTANCE))
+}
+
+inline double BinMean(size_t bin_num, 
+                      const CircularHoughAccumulator& accum) {
+  return accum.votes(bin_num) / accum.accumulator.size();
+}
+
+inline bool MeansDontIntersect(CircularHoughAccumulator& accum) {
+  double mean_difference =
+    BinMean(accum.GetMostVotedBinIndex(), accum) - 
+    BinMean(accum.GetSecondMostVotedBinIndex(), acumm);
+  // Assuming confidence level of 95%
+  double lower_bound = 2.0 * sqrt(1.0 / accum.accumulator.size());
+  return mean_difference >= lower_bound;
+}
+
+vector<Vector2f> NormalComputation::getNormals(const vector<Vector2f>& points) {
+  // For each point we have to randomly sample points within its neighborhood.
+  // Then when we either reach the upper limit of samples, or pass the threshold
+  // of confidence stop.
+  // Pick the most selected bin and set the normal of this point to the average
+  // angle of that bin.
+  // Compute the line using that angle and a point at (1,0).
+  srand(time(NULL));
+  const KDTree<float, 2>* tree =
+    new KDTree<float, 2>(KDTree<2, float>::EigenToKD(points));
+  vector<Vector2f> normals;
+  for (const Vector2f& point : points) {
+    CircularHoughAccumulator accum(BIN_NUMBER);
+    size_t number_of_samples = 0;
+    double neighborhood_size = NEIGHBORHOOD_SIZE;
+    vector<KDNodeValue<float, 2> neighbors;
+    while (neighbors.size() <= 1) {
+      FindNeighborPoints(point, neighborhood_size, &neighbors); 
+      neighborhood_size += NEIGHBORHOOD_STEP_SIZE;
+    }
+    while (number_of_samples < SampleLimit(BIN_NUMBER)) { 
+      // Get a random pair of points using a costless combinatorial ordering. 
+      // TODO: Costless Combinatorial Ordering
+      Vector2f point_1 = neighbors[rand() % neighbors.size()];
+      Vector2f point_2;
+      do {
+        point_2 = neighbors[rand() % neighbors.size()];
+      } while (point_2.x() == point_1.x() && point_2.y() == point_1.y());
+      // Now come up with their normal.
+      Eigen::Hyperplane<float, 2> surface_line =
+        Eigen::Hyperplane<float, 2>::Through(point_1, point_2);
+      Vector2f normal = surface_line.normal();
+      double angle_with_x_axis = acos(normal.dot(Vector2f(1, 0)));
+      accum.AddVote(angle_with_x_axis);
+      if (MeansDontIntersect(accum)) {
+        break; 
+      }
+    }
+    return GetNormalFromAngle(AverageBinAngle(GetMostVotedBinIndex()));
+  }
+}
---
+++ src/normal_computation.h
@@ -0,0 +1,67 @@
+#ifndef SRC_NORMAL_COMPUTATION_H
+#define SRC_NORMAL_COMPUTATION_H
+
+#include <vector>
+
+#include "Eigen/Dense"
+
+#include "./math_util.h"
+
+
+using std::vector;
+using Eigen::Vector2f;
+namespace NormalComputation {
+struct CircularHoughAccumulator {
+  vector<vector<double>> accumulator;
+  const double angle_step;
+  size_t most_voted_bin;
+  size_t second_most_voted_bin;
+
+  // The bin number is the number of bins around the equator.
+  CircularHoughAccumulator(size_t bin_number) : 
+    accumulator(bin_number, vector<T>()), 
+    angle_step(M_2PI / bin_number) {}
+
+  inline Votes(size_t bin_number) {
+    return accumulator[bin_number].size();
+  }
+
+  void AddVote(double angle_in_radians) {
+    angle_in_radians = angle_mod(angle_in_radians);
+    size_t bin_number = round(angle_in_radians / angle_step);
+    if (bin_number > accumulator.size()) {
+      return;
+    }
+    accumulator[bin_number].append(angle_in_radians);
+    if (Votes(most_voted_bin) < Votes(bin_number)) {
+      second_most_voted_bin = most_voted_bin;
+      most_voted_bin = bin_number;
+    } else if (Votes(second_most_voted_bin) < Votes(bin_number)) {
+      second_most_voted_bin = bin_number;
+    }
+  }
+
+  size_t GetMostVotedBinIndex() {
+    return most_voted_bin;
+  }
+
+  size_t GetSecondMostVotedBinIndex() {
+    return second_most_voted_bin;
+  }
+
+  double AverageBinAngle(size_t bin_number) {
+    double avg = 0.0;
+    for (const double angle : accumulator[bin_number]) {
+      avg += angle;
+    }
+    return angle / Votes(bin_number);
+  }
+}
+
+  // Returns a list of normals for the corresponding list of points. 
+  // Normals are unit sized, but because we use meters as our unit, they are 1 meter
+  // long.
+  vector<Vector2f> getNormals(const vector<Vector2f>& points);
+}
+
+#endif
