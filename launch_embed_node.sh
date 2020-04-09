39099d00e0c747c059befffbf40bec22cdc518d8
TheCynosure <jack.github@gmail.com>
2020-04-09 07:31:21 -0400

    Removing uncessary rviz files

--- launch_embed_node.sh
+++
@@ -1,6 +0,0 @@
-# Should this be done with a roslaunch script? Yes,
-# Will it be? No.
-
-cd third_party/learning-loop-closure/point_cloud_embedder
-export ROS_PACKAGE_PATH=$(pwd):$ROS_PACKAGE_PATH
-rosrun point_cloud_embedder point_cloud_embedder.py $1
